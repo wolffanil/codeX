a9083f63a69bf7632cdb755d0db41c47ba634b2a
wolffanil2 <wolffanil@mail.ru>
2023-03-11 22:18:15 +0400

    second commit

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-i5z2.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
