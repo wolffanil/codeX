cab83293f758c870e2cd2b759baec4ef6ee4dabe
wolffanil2 <wolffanil@mail.ru>
2023-04-27 19:22:42 +0400

    end26

--- server/server.js
+++ server/server.js
@@ -17,6 +17,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const detector = new DeviceDetector({
+    clientIndexes: true,
+    deviceIndexes: true,
+    deviceAliasCode: false,
+  });
 const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
 const result = detector.detect(userAgent);
 
